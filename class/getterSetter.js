febc33b63dec8d436e3b6fdf79092718dfdd6e25
vikashraj144 <vikashraj144@gmail.com>
2018-03-30 19:25:00 +0530

    getter setter

---
+++ class/getterSetter.js
@@ -0,0 +1,25 @@
+/* Getter/Setter also directly within classes 
+(and not just within object initializers, as it is possible since ECMAScript 5.1). */
+class Rectangle {
+    constructor(width, height) {
+        this._width = width;
+        this._height = height;
+    }
+    set width(width) {
+        this._width = width;
+    }
+    get width() {
+        return this._width;
+    }
+    set height(height) {
+        this._height = height;
+    }
+    get height() {
+        return this._height;
+    }
+    get area() {
+        return this._width * this._height;
+    }
+};
+var r = new Rectangle(50, 20);
+console.log(r.area) // 1000;
