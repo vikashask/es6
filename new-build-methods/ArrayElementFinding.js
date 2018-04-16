9634061f1328e27bca98cf6b0d33a0e3460fb978
vikashraj144 <vikashraj144@gmail.com>
2018-04-16 08:33:16 +0530

    array element find

---
+++ new-build-methods/ArrayElementFinding.js
@@ -0,0 +1,9 @@
+/* 
+New function for finding an element in an array
+ */
+
+var find = [1, 3, 4, 2].find(x => x > 3); // 4 
+var findIndex = [1, 3, 4, 2].findIndex(x => x > 3); // 2
+
+console.log(find);
+console.log(findIndex);
