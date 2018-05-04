2195fe8dfb3148dd5e0062bd8bbf6f5a9fb64e6b
vikashraj144 <vikashraj144@gmail.com>
2018-05-04 21:35:16 +0530

    file modife

--- rest-Parameter.js
+++
@@ -1,7 +0,0 @@
-/* 
-Aggregation of remaining arguments into single parameter of variadic functions.
- */
-function f(x, y, ...a) { // ...a = "vikash", true, 7 is rest paramater
-    return (x + y) * a.length;
-}
-f(1, 2, "vikash", true, 7) //9;
