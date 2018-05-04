81d3f13e044e697b1e2abec7e07e50a626a48c99
vikashraj144 <vikashraj144@gmail.com>
2018-05-04 21:19:34 +0530

    removed file

--- destructuring/array-Matching.js
+++
@@ -1,9 +0,0 @@
-/* 
-Intuitive and flexible destructuring of Arrays into individual variables during assignment.
- */
-
-var list = [1, 2, 3];
-var [a, , b] = list;
-console.log(a, b);
-[b, a] = [a, b];
-console.log(a, b);
