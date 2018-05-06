d0e70313367da2da3e57a13e6bb04878a4b8364c
vikashraj144 <vikashraj144@gmail.com>
2018-05-06 07:08:15 +0530

    set example added

---
+++ 11.mapSet/set-example1.js
@@ -0,0 +1,11 @@
+var founder = new Map();
+founder.set("fb","mark");
+founder.set("google","larry");
+console.log(founder.size); //2 
+console.log(founder.get("twitter"));// undefinded
+console.log('yahoo has',founder.has("yahoo")); //false
+console.log('google has ',founder.has("google")); //true
+
+for(var [key,value] of founder){
+    console.log(key+' fouded by '+ value);
+}
