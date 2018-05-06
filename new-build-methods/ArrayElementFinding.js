ef237ab6a3e6d3ca40139356a8a544290796f344
vikashraj144 <vikashraj144@gmail.com>
2018-05-06 07:25:06 +0530

    setter getter

--- new-build-methods/ArrayElementFinding.js
+++
@@ -1,9 +0,0 @@
-/* 
-New function for finding an element in an array
- */
-
-var find = [1, 3, 4, 2].find(x => x > 3); // 4 
-var findIndex = [1, 3, 4, 2].findIndex(x => x > 3); // 2
-
-console.log(find);
-console.log(findIndex);
