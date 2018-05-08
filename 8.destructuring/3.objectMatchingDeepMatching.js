6de63cc8c10f88ce3199e341cfd77310c2aa38c2
vikashraj144 <vikashraj144@gmail.com>
2018-05-08 08:12:22 +0530

    deep matching

---
+++ 8.destructuring/3.objectMatchingDeepMatching.js
@@ -0,0 +1,25 @@
+/* 
+Intuitive and flexible destructuring of Objects into individual variables during assignment.
+ */
+
+function getASTNode() {
+    return {
+        one: 1,
+        two: {
+            op: 2
+        },
+        three: 3
+    };
+};
+
+// var { op: a, lhs: { op: b }, rhs: c } = getASTNode();
+var data = {
+    one:a,
+    two: {
+        op: b  // deep matching
+    },
+    three
+} = getASTNode();
+console.log(data.one); // 1
+console.log(data.two.op); // 2 
+console.log(data.three); // 3
