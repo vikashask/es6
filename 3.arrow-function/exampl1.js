866783060f4106ec1478907e2be213511b6727f6
vikash <vikashraj144@gmail.com>
2018-05-19 20:02:00 +0530

    more example added

---
+++ 3.arrow-function/exampl1.js
@@ -0,0 +1,20 @@
+// using 0 parameter
+var docLogEs6 = () => { console.log('document'); };
+docLogEs6(); // #document... <html> ….
+
+const logData = () =>{
+    console.log("test");
+}
+logData();  // test
+
+
+// using two parameter
+const multiplyES6 = (x, y) => { return x * y };
+console.log(multiplyES6(3,4)); // 12
+
+// using three parameter
+const sumEs6 = (a,b,c) =>{ return a+b+c}
+console.log(sumEs6(2,3,4)); // 9
+
+
+
