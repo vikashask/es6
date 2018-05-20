bfe0679f0608fb181947e28da76855997afaa25e
vikash <vikashraj144@gmail.com>
2018-05-20 09:46:27 +0530

    spread operator

--- 4.Extended Parameter Handling/spread-Operator.js
+++ 4.Extended Parameter Handling/spread-Operator.js
@@ -5,10 +5,14 @@ elements and individual function parameters.
 var params = ["vikash", true, 7];
 var other = [1, 2, ...params]; 
 console.log('Spreading other variable',other)// [ 1, 2, "vikash", true, 7 ] 
-function f(x, y, ...a) {
+function spread(x, y, ...a) {
     return (x + y) * a.length;
 }
-f(1, 2, ...params) === 9;
+spread(1, 2, ...params) === 9;
 var str = "raj";
 var chars = [...str]; // [ "r", "a", "j" ]
 console.log('Iteraing string through spread',chars);
+
+let str2 = "Hello";
+console.log(Array.from(str2)); // [ 'H', 'e', 'l', 'l', 'o' ]
+
