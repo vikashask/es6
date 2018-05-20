136f5588aa1f082e7766cbd21771900f47c17750
vikash <vikashraj144@gmail.com>
2018-05-20 09:39:01 +0530

    rest param

--- 4.Extended Parameter Handling/rest-Parameter.js
+++ 4.Extended Parameter Handling/rest-Parameter.js
@@ -1,7 +1,33 @@
 /* 
 Aggregation of remaining arguments into single parameter of variadic functions.
  */
-function f(x, y, ...a) { // ...a = "vikash", true, 7 is rest paramater
+function restFunction(x, y, ...a) { // ...a = "vikash", true, 7 is rest paramater
     return (x + y) * a.length;
 }
-f(1, 2, "vikash", true, 7) //9;
+console.log(restFunction(1, 2, "vikash", true, 7)); //9;
+
+// getting sum of all param
+function sumOfAll(...params) {
+    let sum = 0;
+    for(let param of params){
+        sum += param;
+    }
+    return sum;
+}
+console.log('-----------');
+console.log(sumOfAll(1)); // 1
+console.log(sumOfAll(1,2,3,4)); // 10
+console.log(sumOfAll(1,5,6,0,8,7)); // 27
+
+function multiplyOfParam(...params){
+    let multiply = 1;
+    for (const iterator of params) {
+        multiply *=iterator;
+    }
+    return multiply;
+}
+console.log('---------');
+console.log(multiplyOfParam(1,2)); // 2
+console.log(multiplyOfParam(1,2,3));// 6
+console.log(multiplyOfParam(1,2,4));// 8
+
