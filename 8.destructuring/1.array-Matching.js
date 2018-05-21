307649fbffe81edc0eef9ba3f37cde7a7a318852
vikash <vikashraj144@gmail.com>
2018-05-21 21:26:02 +0530

    array matching

--- 8.destructuring/1.array-Matching.js
+++ 8.destructuring/1.array-Matching.js
@@ -7,3 +7,12 @@ var [a, , b] = list;
 console.log(a, b); //  1 3
 [b, a] = [a, b];
 console.log(a, b); // 3 1
+
+
+// object matching
+var data =  {name:1,id:2};
+var {name,id}=data
+
+console.log(name,'name');
+console.log(id,'id');
+
