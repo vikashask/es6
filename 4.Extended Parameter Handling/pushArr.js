4303ff41c0d0b43c8654fe14711cd868fd70c9c6
vikash <vikashraj144@gmail.com>
2020-10-25 22:12:52 +0530

    push array

---
+++ 4.Extended Parameter Handling/pushArr.js
@@ -0,0 +1,10 @@
+let arr = [];
+let finalArr = [...arr, 3]; // alternative of push
+console.log("finalArr", finalArr); // 3
+
+let listArr = [2, 3, 4, 5, 6];
+let lsArr = [];
+listArr.map((data) => {
+  [...listArr, data];
+});
+console.log("listArr", listArr);
