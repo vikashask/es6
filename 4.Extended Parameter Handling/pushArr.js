03991ac2d7671807e8a2d2e403f1988b07827c29
vikash verma <113046682+vikashverma144@users.noreply.github.com>
2023-02-05 11:37:02 +0530

    push in array

--- 4.Extended Parameter Handling/pushArr.js
+++ 4.Extended Parameter Handling/pushArr.js
@@ -5,6 +5,6 @@ console.log("finalArr", finalArr); // 3
 let listArr = [2, 3, 4, 5, 6];
 let lsArr = [];
 listArr.map((data) => {
-  [...listArr, data];
+  [...listArr, data]; // listArr.push(data)
 });
 console.log("listArr", listArr);
--- 4.Extended Parameter Handling/spread-Operator.js
+++ 4.Extended Parameter Handling/spread-Operator.js
@@ -3,16 +3,15 @@ Spreading of elements of an iterable collection (like an array or even a string)
 elements and individual function parameters.
  */
 var params = ["vikash", true, 7];
-var other = [1, 2, ...params]; 
-console.log('Spreading other variable',other)// [ 1, 2, "vikash", true, 7 ] 
+var other = [1, 2, ...params];
+console.log("Spreading other variable", other); // [ 1, 2, "vikash", true, 7 ]
 function spread(x, y, ...a) {
-    return (x + y) * a.length;
+  return (x + y) * a.length;
 }
 spread(1, 2, ...params) === 9;
 var str = "raj";
 var chars = [...str]; // [ "r", "a", "j" ]
-console.log('Iteraing string through spread',chars);
+console.log("Iterating string through spread", chars);
 
 let str2 = "Hello";
 console.log(Array.from(str2)); // [ 'H', 'e', 'l', 'l', 'o' ]
-
